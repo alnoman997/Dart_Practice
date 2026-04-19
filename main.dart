de04ac657060fd027165297ba8ef5600ef7cd5bb
alnoman997 <alnoman997@gmail.com>
2026-04-19 15:04:29 +0600

    asynchronous

--- main.dart
+++ main.dart
@@ -1,5 +1,14 @@
+//Asychronous programming practice.
 
+void main() {
+  fetchData();
+}
 
+void fetchData() async {
+  print("Fetching data...");
+  await Future.delayed(Duration(seconds: 4));
+  print("Data fetched successfully.");
+}
 
 
 /*
